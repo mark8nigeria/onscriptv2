6a5a40f51e581df31078ce81248cc6065b742b50
samuelkime267 <samuelkime7@gmail.com>
2025-09-25 21:49:59 +0100

    fix onchain error

--- client.ts
+++ client.ts
@@ -1,7 +1,7 @@
 import { createPublicClient, http } from "viem";
-import { baseSepolia } from "viem/chains";
+import { base } from "viem/chains";
 
 export const publicClient = createPublicClient({
-  chain: baseSepolia,
+  chain: base,
   transport: http(),
 });
