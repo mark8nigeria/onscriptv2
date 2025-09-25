a83f6b088238fa8d9bf757582c5bd4c6516595fc
samuelkime267 <samuelkime7@gmail.com>
2025-09-25 19:38:27 +0100

    generated prisma

--- prisma/generated/prisma/edge.js
+++ prisma/generated/prisma/edge.js
@@ -430,7 +430,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
--- prisma/generated/prisma/index.js
+++ prisma/generated/prisma/index.js
@@ -431,7 +431,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
